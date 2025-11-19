51ecdcf95e208a5eee4f876ed82ef0ba50103758
nargisk2658 <nargisk7170@gmail.com>
2025-11-19 22:52:26 +0530

    Update T1.py

--- Assignment2/T1.py
+++ Assignment2/T1.py
@@ -1,3 +1,8 @@
+'''Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.'''
+
 num = int(input("Enter an integar number: "))
 if num % 2 :
     print (num, "is an odd number.")
