bd32e312e7ef0922c3b72457fdfe048a864dbb98
Nargis Khatoon <nargisk7170@gmail.com>
2026-02-09 18:58:35 +0530

    Add files via upload

--- Assignment4/task1.py
+++ Assignment4/task1.py
@@ -1,14 +1,29 @@
-''' Write a Python program that:
+'''Task 1: Read a File and Handle Errors 
+Problem Statement:  Write a Python program that:
 1.   Opens and reads a text file named sample.txt.
 2.   Prints its content line by line.
 3.   Handles errors gracefully if the file does not exist.
+ 
+Expected Output:
+
+If the file exists:
+Reading file content :
+Line 1:  This is a sample text file.
+Line 2: It contains multiple lines.
+
+If the file does not exist:
+Error: The file 'sample.txt' does not exist.
+provide me the pythoņcode
 '''
+
 try:
-    
-    with open("sample.txt", "r") as file:
-        for line in file:
-            print(line.strip())
-
+    print("Reading file content :")
+    file = open("sample.txt", "r")
+    line_no = 1
+    for line in file:
+        print("Line", line_no, ":", line.strip())
+        line_no += 1
+    file.close()
 except FileNotFoundError:
-
     print("Error: The file 'sample.txt' does not exist.")
+
